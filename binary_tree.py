01cb2ed8869c77a186fabdd47886c97f46eacb3c
lparsenadze <lia.parsenadze@dataart.com>
2022-11-18 19:36:51 +0400

    Added isSubtree func to binary tree DS implementation

--- binary_tree.py
+++ binary_tree.py
@@ -21,10 +21,35 @@ class Node:
             self.left.subtree_first()
         else:
             return self
+
+### TODO
+# the below function is correct but needs restructuring
+# need something cleaner.
+# Also Node class must only have initialization 
+# and all other tree operations must be left as independant functions or
+# methods of another class.
+# The setting above is confusing
+##
             
-    def subtree_insert_after(self, x):
-        assert x >= self.value
-    
+def is_subtree(t1, t2):
+    def _same_tree(t1, t2):
+        if not t1 and not t2:
+            return True
+            
+        if t1 and t2 and t1.value == t2.value:
+            return (_same_tree(t1.left, t2.left)) and
+                   (_same_tree*t1.right, t2.right))
+       
+        return False
+
+    if not t2: return True
+    if not t1: return False
+    if _same_tree(self., t2):
+        return True
+    return (is_subtree(t1.left, t2) or
+            is_subtree(t1.right, t2))
+
+ 
 tree = Node(5)
 
 tree.insert(7)
