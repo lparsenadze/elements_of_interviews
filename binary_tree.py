5f0d41492acd1bc7d1a79e731cade2932e59aa5d
lparsenadze <lia.parsenadze@dataart.com>
2022-11-19 17:37:00 +0400

    added BT class

--- binary_tree.py
+++ binary_tree.py
@@ -3,55 +3,61 @@ class Node:
         self.value = value
         self.right = right
         self.left = left
-        
-    def insert(self, node):
-        if node.value < self.value:
-            if self.left is None:
-                self.left = node
-            else:
-                self.left.insert(node)
-        else:
-            if self.right is None:
-                self.right = node
-            else:
-                self.right.insert(node)
-                
-    def subtree_first(self):
-        if self.left:
-            self.left.subtree_first()
-        else:
-            return self
 
-### TODO
-# the below function is correct but needs restructuring
-# need something cleaner.
-# Also Node class must only have initialization 
-# and all other tree operations must be left as independant functions or
-# methods of another class.
-# The setting above is confusing
-##
-            
-def is_subtree(t1, t2):
-    def _same_tree(t1, t2):
-        if not t1 and not t2:
+
+class BinaryTree:
+    def __init__(self, rootNode):
+        self.root = rootNode
+
+    
+    def insert(self, root, node):
+        if node.value < root.value:
+            if root.left is None:
+                root.left = node
+            else:
+                self.insert(root.left, node)
+        else:
+            if root.right is None:
+                root.right = node
+            else:
+                self.insert(root.right, node)
+
+    #@staticmethod
+    def is_subtree(self, t1, t2):
+        def _same_tree(t1, t2):
+            if not t1 and not t2:
+                return True
+   
+            if t1 and t2 and t1.value == t2.value:
+                return (_same_tree(t1.left, t2.left) and
+                       _same_tree(t1.right, t2.right))
+   
+            return False
+   
+        if not t2: return True
+        if not t1: return False
+        if _same_tree(t1, t2):
             return True
-            
-        if t1 and t2 and t1.value == t2.value:
-            return (_same_tree(t1.left, t2.left)) and
-                   (_same_tree*t1.right, t2.right))
-       
-        return False
+        return (self.is_subtree(t1.left, t2) or
+                self.is_subtree(t1.right, t2))
+   
+    #@staticmethod
+    def subtree_first(self, node):
+        if node.left is None:
+            return node
+        self.subtree_first(node.left)
+        
+    def subtree_insert_after(self, node, new):
+        if not node.right:
+            node.right = new
+        else:
+            successor = self.subtree_first(node.right)
+            successor.left = new
 
-    if not t2: return True
-    if not t1: return False
-    if _same_tree(self., t2):
-        return True
-    return (is_subtree(t1.left, t2) or
-            is_subtree(t1.right, t2))
-
- 
-tree = Node(5)
-
-tree.insert(7)
-tree.insert(4)
-tree.subtree_first()
+rootNode = Node(4)
+tree = BinaryTree(rootNode)
+testNode = Node(7)
+tree.insert(tree.root, testNode)
+#tree.insert(tree.root, Node(6))
+tree.insert(tree.root, Node(6))
+tree.subtree_insert_after(testNode, Node(5))
